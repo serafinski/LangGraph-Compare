94bdc555df04b88a4acacbe8758a4da23852ce11
Tomasz Serafiński <work.serafinski@gmail.com>
2024-10-24 19:54:27 +0200

    added total_tokens to the csv generation

--- log_to_csv_skip.py
+++ log_to_csv_skip.py
@@ -19,10 +19,37 @@ with open(csv_filename, mode='w', newline='') as csv_file:
     for log_entry in logs:
         case_id = log_entry.get('thread_ID')
         timestamp = log_entry['checkpoint'].get('ts')
+
+        # Domyślna wartość jeśli nie znajdziemy total_tokens
         cost = 0
 
         metadata = log_entry.get('metadata', {})
         writes = metadata.get('writes', {})
+
+        # Sprawdzenie, czy writes nie jest None i jest słownikiem
+        if writes and isinstance(writes, dict):
+            # Wyciągnięcie pierwszego klucza jako node'a (np. 'chatbot_node')
+            node_name = list(writes.keys())[0]
+            node_data = writes.get(node_name, {})
+
+            # Sprawdzenie, czy node_data jest słownikiem
+            if isinstance(node_data, dict):
+                messages = node_data.get('messages', [])
+                for message in messages:
+                    # Sprawdzenie, czy message to słownik
+                    if isinstance(message, dict) and 'kwargs' in message:
+                        kwargs = message.get('kwargs', {})
+                        usage_metadata = kwargs.get('usage_metadata', {})
+                        # Wyciągnięcie total_tokens z usage_metadata
+                        cost = usage_metadata.get('total_tokens', 0)
+                        break  # Zatrzymanie po znalezieniu pierwszej wartości
+                    elif isinstance(message, list) and len(message) > 1 and isinstance(message[1], dict):
+                        # Obsługa przypadku, gdy message jest listą
+                        kwargs = message[1].get('kwargs', {})
+                        usage_metadata = kwargs.get('usage_metadata', {})
+                        # Wyciągnięcie total_tokens z usage_metadata
+                        cost = usage_metadata.get('total_tokens', 0)
+                        break  # Zatrzymanie po znalezieniu pierwszej wartości
 
         # Wyciągnięcie activity i org:resource (pierwszy klucz w 'writes')
         if writes:
