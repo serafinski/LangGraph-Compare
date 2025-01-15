dd4a2fb1afe1dac8a42ee849a231ee97dbd08ab1
Tomasz Serafiński <work.serafinski@gmail.com>
2025-01-15 21:38:13 +0100

    reflexion init

---
+++ examples/reflexion/reflexion.py
@@ -0,0 +1,239 @@
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_core.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.tools import StructuredTool
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+from langchain_openai import ChatOpenAI
+
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import END, StateGraph, START
+from langgraph.graph.message import add_messages
+
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from pydantic import ValidationError, BaseModel, Field
+
+import datetime
+import json
+
+from dotenv import load_dotenv
+
+from langgraph_compare import *
+
+exp = create_experiment("reflexion")
+memory = exp.memory
+
+load_dotenv()
+
+# TOOLS
+llm = ChatOpenAI(model="gpt-4o-mini")
+search = TavilySearchAPIWrapper()
+tavily_tool = TavilySearchResults(search=search, max_results=5)
+
+# INITIAL RESPONDER
+class Reflection(BaseModel):
+    missing: str = Field(description="Critique of what is missing.")
+    superfluous: str = Field(description="Critique of what is superfluous")
+
+
+class AnswerQuestion(BaseModel):
+    """Answer the question. Provide an answer, reflection, and then follow up with search queries to improve the answer."""
+
+    answer: str = Field(description="~250 word detailed answer to the question.")
+    reflection: Reflection = Field(description="Your reflection on the initial answer.")
+    search_queries: list[str] = Field(
+        description="1-3 search queries for researching improvements to address the critique of your current answer."
+    )
+
+class ResponderWithRetries:
+    def __init__(self, runnable, validator):
+        self.runnable = runnable
+        self.validator = validator
+
+    def respond(self, state: dict):
+        response = []
+        for attempt in range(3):
+            response = self.runnable.invoke(
+                {"messages": state["messages"]}, {"tags": [f"attempt:{attempt}"]}
+            )
+            try:
+                self.validator.invoke(response)
+                return {"messages": response}
+            except ValidationError as e:
+                state = state + [
+                    response,
+                    ToolMessage(
+                        content=f"{repr(e)}\n\nPay close attention to the function schema.\n\n"
+                        + self.validator.schema_json()
+                        + " Respond by fixing all validation errors.",
+                        tool_call_id=response.tool_calls[0]["id"],
+                    ),
+                ]
+        return {"messages": response}
+
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are expert researcher.
+Current time: {time}
+
+1. {first_instruction}
+2. Reflect and critique your answer. Be severe to maximize improvement.
+3. Recommend search queries to research information and improve your answer.""",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "user",
+            "\n\n<system>Reflect on the user's original question and the"
+            " actions taken thus far. Respond using the {function_name} function.</reminder>",
+        ),
+    ]
+).partial(
+    time=lambda: datetime.datetime.now().isoformat(),
+)
+initial_answer_chain = actor_prompt_template.partial(
+    first_instruction="Provide a detailed ~250 word answer.",
+    function_name=AnswerQuestion.__name__,
+) | llm.bind_tools(tools=[AnswerQuestion])
+validator = PydanticToolsParser(tools=[AnswerQuestion])
+
+first_responder = ResponderWithRetries(
+    runnable=initial_answer_chain, validator=validator
+)
+
+example_question = "Why is reflection useful in AI?"
+initial = first_responder.respond(
+    {"messages": [HumanMessage(content=example_question)]}
+)
+
+# REVISION
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+
+# Extend the initial answer schema to include references.
+# Forcing citation in the model encourages grounded responses
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question. Provide an answer, reflection,
+
+    cite your reflection with references, and finally
+    add search queries to improve the answer."""
+
+    references: list[str] = Field(
+        description="Citations motivating your updated answer."
+    )
+
+
+revision_chain = actor_prompt_template.partial(
+    first_instruction=revise_instructions,
+    function_name=ReviseAnswer.__name__,
+) | llm.bind_tools(tools=[ReviseAnswer])
+revision_validator = PydanticToolsParser(tools=[ReviseAnswer])
+
+revisor = ResponderWithRetries(runnable=revision_chain, validator=revision_validator)
+
+
+revised = revisor.respond(
+    {
+        "messages": [
+            HumanMessage(content=example_question),
+            initial["messages"],
+            ToolMessage(
+                tool_call_id=initial["messages"].tool_calls[0]["id"],
+                content=json.dumps(
+                    tavily_tool.invoke(
+                        {
+                            "query": initial["messages"].tool_calls[0]["args"][
+                                "search_queries"
+                            ][0]
+                        }
+                    )
+                ),
+            ),
+        ]
+    }
+)
+revised["messages"]
+
+# TOOL NODE
+def run_queries(search_queries: list[str], **kwargs):
+    """Run the generated queries."""
+    return tavily_tool.batch([{"query": query} for query in search_queries])
+
+
+tool_node = ToolNode(
+    [
+        StructuredTool.from_function(run_queries, name=AnswerQuestion.__name__),
+        StructuredTool.from_function(run_queries, name=ReviseAnswer.__name__),
+    ]
+)
+
+
+# CONSTRUCT GRAPH
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+MAX_ITERATIONS = 5
+builder = StateGraph(State)
+builder.add_node("draft", first_responder.respond)
+
+
+builder.add_node("execute_tools", tool_node)
+builder.add_node("revise", revisor.respond)
+# draft -> execute_tools
+builder.add_edge("draft", "execute_tools")
+# execute_tools -> revise
+builder.add_edge("execute_tools", "revise")
+
+# Define looping logic:
+def _get_num_iterations(state: list):
+    i = 0
+    for m in state[::-1]:
+        if m.type not in {"tool", "ai"}:
+            break
+        i += 1
+    return i
+
+
+def event_loop(state: list):
+    # in our case, we'll just stop after N plans
+    num_iterations = _get_num_iterations(state["messages"])
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
+
+
+# revise -> execute_tools OR end
+builder.add_conditional_edges("revise", event_loop, ["execute_tools", END])
+builder.add_edge(START, "draft")
+graph = builder.compile(checkpointer=memory)
+
+user_input = {"messages": [("user", "How should we handle the climate crisis?")]}
+
+print()
+run_multiple_iterations(graph, 1, 3, user_input)
+print()
+
+graph_config = GraphConfig(
+    nodes=["draft", "execute_tools", "revise"]
+)
+
+prepare_data(exp, graph_config)
+
+# ANALIZA
+print()
+event_log = load_event_log(exp)
+print_analysis(event_log)
+print()
+
+generate_artifacts(event_log, graph, exp)
